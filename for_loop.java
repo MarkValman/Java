ecea328fbb139f644d90330c56250a8affa7bd98
Mark Valman <markvalman@Marks-Air.lan>
2021-12-21 22:45:20 +0200

    Java For & While Loop

---
+++ for_loop.java
@@ -0,0 +1,11 @@
+public class for_loop {
+   public static void main(String[] args) {
+
+    for(int i = 10; i>=0; i-=2) {
+        System.out.println(i);
+    }
+
+    System.out.println("Happy New Year!");
+
+   }
+}
