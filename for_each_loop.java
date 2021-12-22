652c232b53d165d7abe6fa763483e6a4810a152c
Mark Valman <markvalman@Marks-Air.lan>
2021-12-22 21:12:51 +0200

    For Each Loop

---
+++ for_each_loop.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class for_each_loop {
+
+    public static void main(String[] args) { 
+
+        String[] animals = {"cat", "dog", "carrot"};
+
+        // ArrayList<String> animals = new ArrayList<String>();
+
+        // animals.add("koshara");
+        // animals.add("pes");
+        // animals.add("frog");
+        
+        for(String i : animals) {
+            System.out.println(i);
+        }
+
+    }
+}
+
