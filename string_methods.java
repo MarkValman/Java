acb218a06320d0dc4ee09878f4551d6f42fb5c97
Mark Valman <markvalman@Marks-Air.lan>
2021-12-22 19:58:17 +0200

    string_methods

---
+++ string_methods.java
@@ -0,0 +1,20 @@
+public class string_methods {
+    
+    public static void main(String[] args) {
+
+        String name = "Mark";
+
+        // boolean result = name.equalsIgnoreCase("mark");
+        // int result = name.length();
+        // char result = name.charAt(3);
+        // int result = name.indexOf("a");
+        // boolean result = name.isEmpty();
+        // String result = name.toUpperCase();
+        // String result = name.toLowerCase();
+        // String result = name.trim();
+        String result = name.replace('a', 'e');
+
+        System.out.println(result);
+    }
+
+}
