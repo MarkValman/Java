92753512f9953df718d33ccd042f30ba09acc1ae
Mark Valman <markvalman@Marks-Air.lan>
2021-12-23 21:49:27 +0200

    Method

---
+++ method.java
@@ -0,0 +1,29 @@
+public class method {
+
+    public static void main(String[] args) {
+
+        int x = 3;
+        int y = 4;
+
+        System.out.println(method_2(x,y));
+
+    //     String name = "Mark";
+    //     int age = 21;
+
+    //     shalom(name, age);
+
+    // }
+
+    // static void shalom(String name, int age) {
+    //     System.out.println("Shalom!!!" + name + " " + age);
+
+    }
+
+    static int method_2(int x, int y) {
+
+        // z = x + y;
+        // return z;
+        return x + y;
+    }
+    
+}
